4b813753da597f72b41ca78c6fa60166732786b0
db y99 <db@y99.vn>
2025-08-24 10:29:49 +0000

    Nếu dùng font này thì font weight to lên tại vì nếu nhìn như này thì fon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const playfair = Playfair_Display({
   subsets: ['latin'],
   variable: '--font-playfair',
   display: 'swap',
+  weight: ['400', '500', '600', '700', '800', '900'],
 });
 
 const ptSans = PT_Sans({
