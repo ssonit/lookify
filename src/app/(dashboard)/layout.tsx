d73c49fda38a99e3ac20103c8c877ee7a84e9a1c
db y99 <db@y99.vn>
2025-08-25 09:25:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DashboardNav>
    
    wi

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -31,11 +31,11 @@ export default function DashboardLayout({
     <div className="flex min-h-screen w-full flex-col">
         <Header />
         <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-           <div className="mx-auto grid w-full max-w-6xl gap-2">
-            <h1 className="text-3xl font-semibold">Bảng điều khiển</h1>
-          </div>
            <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-            <DashboardNav items={navItems} />
+            <div className="flex flex-col gap-6">
+                <h1 className="text-3xl font-semibold">Bảng điều khiển</h1>
+                <DashboardNav items={navItems} />
+            </div>
             <div className="grid gap-6">
                 {children}
             </div>
