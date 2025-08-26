7b173d8661938f88d64d2b27b3ebc7c8ffcc7aab
db y99 <db@y99.vn>
2025-08-26 08:55:48 +0000

    người dùng cũng cần avatar

--- src/app/(dashboard)/dashboard/users/page.tsx
+++ src/app/(dashboard)/dashboard/users/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import {
@@ -16,6 +17,7 @@ import { Edit, Shield, Trash } from "lucide-react";
 import { Pagination } from "@/components/pagination";
 import React from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 
 export default function DashboardUsersPage() {
@@ -41,6 +43,7 @@ export default function DashboardUsersPage() {
         <Table>
           <TableHeader>
             <TableRow>
+              <TableHead className="w-[5%]">Avatar</TableHead>
               <TableHead className="w-[20%]">Tên</TableHead>
               <TableHead>Email</TableHead>
               <TableHead className="w-[10%]">Vai trò</TableHead>
@@ -52,6 +55,12 @@ export default function DashboardUsersPage() {
           <TableBody>
             {paginatedUsers.map((user) => (
               <TableRow key={user.id}>
+                <TableCell>
+                  <Avatar>
+                    <AvatarImage src={user.avatar} alt={user.name} />
+                    <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+                  </Avatar>
+                </TableCell>
                 <TableCell className="font-medium">{user.name}</TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell>
